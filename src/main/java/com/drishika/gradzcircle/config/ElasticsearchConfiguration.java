851b4e84b0449e3ea69115052bcc7a5c0dd08388
Abhinav Prakash <abhinav@Abhinavs-iMac.local>
2018-12-06 20:38:46 +0800

    removed commented code for ES and aysnch match

--- src/main/java/com/drishika/gradzcircle/config/ElasticsearchConfiguration.java
+++ src/main/java/com/drishika/gradzcircle/config/ElasticsearchConfiguration.java
@@ -1,11 +1,15 @@
 package com.drishika.gradzcircle.config;
 
 import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
-import org.elasticsearch.client.Client;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.shield.ShieldPlugin;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
@@ -21,35 +25,40 @@ public class ElasticsearchConfiguration {
 	private final Logger log = LoggerFactory.getLogger(ElasticsearchConfiguration.class);
 
 	// USE in DEV
-	@Bean
+	/*@Bean
 	public ElasticsearchTemplate elasticsearchTemplate(Client client,
 			Jackson2ObjectMapperBuilder jackson2ObjectMapperBuilder) {
 		return new ElasticsearchTemplate(client,
 				new CustomEntityMapper(jackson2ObjectMapperBuilder.createXmlMapper(false).build()));
 	}
-
+*/
 	/* USED for PROD */
-	/*
-	 * @Bean public ElasticsearchTemplate
-	 * elasticsearchTemplate(Jackson2ObjectMapperBuilder
-	 * jackson2ObjectMapperBuilder) { return new ElasticsearchTemplate(client(), new
-	 * CustomEntityMapper(jackson2ObjectMapperBuilder.createXmlMapper(false).build()
-	 * )); }
-	 * 
-	 * @Bean public TransportClient client() { TransportClient client = null;
-	 * boolean enableSsl = true; try { Settings settings =
-	 * Settings.builder().put("cluster.name", "3c324b76257b9c008f22f0a038088ec1")
-	 * .put("transport.ping_schedule", "5s") // .put("transport.sniff", false)
-	 * .put("action.bulk.compress", false).put("shield.transport.ssl", enableSsl)
-	 * .put("request.headers.X-Found-Cluster", "3c324b76257b9c008f22f0a038088ec1")
-	 * .put("shield.user", "gradzcircle:oc4ehtn0n34").build(); client =
-	 * TransportClient.builder().addPlugin(ShieldPlugin.class).settings(settings).
-	 * build() .addTransportAddress(new InetSocketTransportAddress(
-	 * InetAddress.getByName(
-	 * "3c324b76257b9c008f22f0a038088ec1.us-east-1.aws.found.io"), 9343)); } catch
-	 * (UnknownHostException e) { log.error("Error creating Elastic Client {}",
-	 * e.getMessage()); } return client; }
-	 */
+	
+	@Bean
+	public ElasticsearchTemplate elasticsearchTemplate(Jackson2ObjectMapperBuilder jackson2ObjectMapperBuilder) {
+		return new ElasticsearchTemplate(client(),
+				new CustomEntityMapper(jackson2ObjectMapperBuilder.createXmlMapper(false).build()));
+	}
+
+	@Bean
+	public TransportClient client() {
+		TransportClient client = null;
+		boolean enableSsl = true;
+		try {
+			Settings settings = Settings.builder().put("cluster.name", "3c324b76257b9c008f22f0a038088ec1")
+					.put("transport.ping_schedule", "5s") // .put("transport.sniff", false)
+					.put("action.bulk.compress", false).put("shield.transport.ssl", enableSsl)
+					.put("request.headers.X-Found-Cluster", "3c324b76257b9c008f22f0a038088ec1")
+					.put("shield.user", "gradzcircle:oc4ehtn0n34").build();
+			client = TransportClient.builder().addPlugin(ShieldPlugin.class).settings(settings).build()
+					.addTransportAddress(new InetSocketTransportAddress(
+							InetAddress.getByName("3c324b76257b9c008f22f0a038088ec1.us-east-1.aws.found.io"), 9343));
+		} catch (UnknownHostException e) {
+			log.error("Error creating Elastic Client {}", e.getMessage());
+		}
+		return client;
+	}
+	
 	public class CustomEntityMapper implements EntityMapper {
 
 		private ObjectMapper objectMapper;
