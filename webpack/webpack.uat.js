0f8ecf9fd97999248146af124d1778e098ffede4
Abhinav Prakash <abhinav@Abhinavs-iMac.local>
2018-12-09 15:46:47 +0800

    fixing build issues for uat deployment

--- webpack/webpack.uat.js
+++ webpack/webpack.uat.js
@@ -2,8 +2,9 @@ const webpack = require('webpack');
 const webpackMerge = require('webpack-merge');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const Visualizer = require('webpack-visualizer-plugin');
-const ngcWebpack = require('ngc-webpack');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
+const AngularCompilerPlugin = require('@ngtools/webpack').AngularCompilerPlugin;
 const path = require('path');
 
 const utils = require('./utils.js');
@@ -29,33 +30,10 @@ module.exports = webpackMerge(commonConfig({ env: ENV }), {
         chunkFilename: 'app/[id].[hash].chunk.js'
     },
     module: {
-        rules: [
-        	{
-            test: /\.ts$/,
-            use: [
-                {
-                    loader: 'awesome-typescript-loader',
-                    options: {
-                        configFileName: 'tsconfig-aot.json'
-                    },
-                },
-                { loader: 'angular2-template-loader' }
-            ],
-            exclude: ['node_modules/generator-jhipster']
-        },
-        {
-            test: /\.ts$/,
-            use: [
-                {
-                    loader: 'ngc-webpack',
-                    options: {
-                        disable: false,
-                        tsConfigPath: 'tsconfig-aot.json'
-                    }
-                }
-            ],
-            exclude: /(polyfills\.ts|vendor\.ts|Reflect\.ts)/
-        },
+        	rules: [{
+                test: /(?:\.ngfactory\.js|\.ngstyle\.js|\.ts)$/,
+                use: [ '@ngtools/webpack' ]
+            },
         {
             test: /\.css$/,
             loaders: ['to-string-loader', 'css-loader'],
@@ -114,14 +92,18 @@ module.exports = webpackMerge(commonConfig({ env: ENV }), {
                 }
             }
         }),
-     new ngcWebpack.NgcWebpackPlugin({
-            disabled: false,
-            tsConfig: utils.root('tsconfig-aot.json'),
-            resourceOverride: ''
+        new AngularCompilerPlugin({
+            mainPath: utils.root('src/main/webapp/app/app.main.ts'),
+            tsConfigPath: utils.root('tsconfig-aot.json'),
+            sourceMap: true
         }),
         new webpack.LoaderOptionsPlugin({
             minimize: true,
             debug: false
+        }),
+        new WorkboxPlugin.GenerateSW({
+          clientsClaim: true,
+          skipWaiting: true,
         })
     ]
 });
