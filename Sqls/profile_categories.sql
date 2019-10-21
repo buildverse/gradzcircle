1a51ffd3f0237ce8e95443c832e20dfdcfb7e6aa
Abhinav Prakash <prakash.abhinav1978@gmail.com>
2019-10-22 00:16:35 +0800

    added logos and favicon

--- Sqls/profile_categories.sql
+++ Sqls/profile_categories.sql
@@ -1,12 +1,13 @@
 ﻿INSERT INTO profile_category(
 	id, category_name, weightage)
-	VALUES (1, 'education', 50),
+	VALUES (1, 'education', 30),
 	(2, 'personalDetails', 15),
 	(3, 'experience', 15),
 	(4, 'certification', 5),
 	(5, 'language', 5),
 	(6, 'nonAcademic', 5),
-	(7, 'basicDetails', 5);
+	(7, 'basicDetails', 5),
+	(8,'skill',20;
 
 
 
